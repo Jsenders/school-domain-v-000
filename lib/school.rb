f03d23f5d1bde4f5822542ce85bec82454d24c6d
Joseph Senders <jasenders@gmail.com>
2018-08-13 20:23:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,23 @@ class School
     @name = name
     @roster= {}
   end
+  def add_student(names,grade)
+       if @roster[grade]
+         @roster[grade] << names
+       else
+         @roster[grade] = [names]
+       end
+    end
 
+    def grade(grade)
+      @roster[grade]
+    end
+
+    def sort
+      sorted_array = @roster.keys.sort
+      new_roster = {}
+      sorted_array.each { |index| new_roster[index] = @roster[index].sort }
+      @roster=new_roster
+    end
 
 end
